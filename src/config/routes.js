75b8d6d4265007dc003d17406ebe27fc6f413cb0
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-13 14:05:21 +0100

    added GET login in routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,13 +4,14 @@ const router = Router();
 
 //TO DO: import controllers
 const homeController = require('../controllers/homeController');
+const userController = require('../controllers/userController');
 //TO DO: Endpoints and actions 
 
 //HOME
 router.get('/', homeController.getHome);
 //AUTH
 //Login
-
+router.get('/users/login', userController.getLogin);
 //Register
 
 //Logout
