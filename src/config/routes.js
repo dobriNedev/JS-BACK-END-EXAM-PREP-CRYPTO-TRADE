0af5696869440ecb5a99e822c7d8487c6023a31c
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-13 12:52:33 +0100

    routes.jsinitial setup

---
+++ src/config/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const Router = express.Router;
+const router = Router();
+
+//TO DO: import controllers
+
+//TO DO: Endpoints and actions 
+
+//HOME
+
+//AUTH
+//Login
+
+//Register
+
+//Logout
+
+//OTHER
+
+module.exports = router;
