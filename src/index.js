955d3fa9cbaf78795d59926f6341e9876c87ec98
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-13 13:17:08 +0100

    TO DO: added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const initDB = require('./config/database');
 const viewEngineSetup = require('./config/viewEngine');
 const routes = require('./config/routes');
 const { PORT } = require('./config/constants');
+//TO DO: import authMiddleware
 
 const app = express();
 viewEngineSetup(app);
@@ -11,7 +12,7 @@ app.use(express.static('src/static'));
 app.use(express.urlencoded({
     extended: false
 }));
-
+//TO DO: use authMiddleware
 app.use(routes);
 
 initDB()
