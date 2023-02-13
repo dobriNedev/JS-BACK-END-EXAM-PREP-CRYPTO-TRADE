258328c50c5fb6a17fe920f69116cf6038c2b696
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-13 13:00:19 +0100

    contants.js added

---
+++ src/config/constants.js
@@ -0,0 +1,5 @@
+exports.PORT = 3000;
+//change <colection> with the desired name of collection for the project
+exports.DB_URI = 'mongodb://localhost:27017/<colection>'; 
+
+exports.SECRET = 'Somespecialsecretsecretofmysecret';
