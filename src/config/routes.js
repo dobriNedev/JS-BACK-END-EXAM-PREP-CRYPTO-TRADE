063a2a313dfbdb3c60d404403f57312deee101b5
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-13 13:55:10 +0100

    route for home

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,11 +3,11 @@ const Router = express.Router;
 const router = Router();
 
 //TO DO: import controllers
-
+const homeController = require('../controllers/homeController');
 //TO DO: Endpoints and actions 
 
 //HOME
-
+router.get('/', homeController.getHome);
 //AUTH
 //Login
 
