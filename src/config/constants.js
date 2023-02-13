0408e4b9f1ae15584eae885c724cff14cf650a6c
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-13 13:44:46 +0100

    bcrypt dependencies

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 exports.PORT = 3000;
 //change <colection> with the desired name of collection for the project
-exports.DB_URI = 'mongodb://localhost:27017/<colection>'; 
+exports.DB_URI = 'mongodb://localhost:27017/cryptoTrade'; 
 
 exports.SECRET = 'Somespecialsecretsecretofmysecret';
