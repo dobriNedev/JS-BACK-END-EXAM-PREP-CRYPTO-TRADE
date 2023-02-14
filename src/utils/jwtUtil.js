1e6be59b6f8c496667a9253dc8c2f6709a6d885e
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-14 19:10:18 +0100

    promisifying jwt .sign and .verify

--- src/utils/jwtUtil.js
+++ src/utils/jwtUtil.js
@@ -1,17 +1,5 @@
 const jwt = require('jsonwebtoken');
+const util = require('util');
 
-
-// exports.jwtSign = util.promisify(jwt.sign);
-exports.jwtSign = function(payload, secret) {
-    let promise = new Promise((resolve, reject) => {
-        jwt.sign(payload, secret, function(err, token) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(token);
-            }
-        });
-    });
-
-    return promise;
-};
+exports.jwtSign = util.promisify(jwt.sign);
+exports.jwtVerify = util.promisify(jwt.verify);
