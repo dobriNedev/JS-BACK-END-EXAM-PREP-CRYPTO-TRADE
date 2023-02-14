bf1955ebe01d03213454b6214c943e9db1b6e493
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-14 19:15:52 +0100

    responsive navigation

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,8 @@
 exports.PORT = 3000;
+
 //change <colection> with the desired name of collection for the project
 exports.DB_URI = 'mongodb://localhost:27017/cryptoTrade'; 
 
 exports.SECRET = 'Somespecialsecretsecretofmysecret';
+
+exports.COOKIE_TOKEN_NAME = 'authToken';
