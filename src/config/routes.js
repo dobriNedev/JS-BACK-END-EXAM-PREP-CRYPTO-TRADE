7422d076bdf5dd53477414fbf75cecc520daad95
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-14 13:24:36 +0100

    crypo/catalog GET added

--- src/config/routes.js
+++ src/config/routes.js
@@ -5,17 +5,19 @@ const router = Router();
 //TO DO: import controllers
 const homeController = require('../controllers/homeController');
 const userController = require('../controllers/userController');
+const cryptoController = require('../controllers/cryptoContorller');
 //TO DO: Endpoints and actions 
 
 //HOME
 router.get('/', homeController.getHome);
-//AUTH
+//AUTH-USER
 //Login
 router.get('/users/login', userController.getLogin);
 //Register
 router.get('/users/register', userController.getRegister);
 //Logout
-
+//CRYPTO
+router.get('/crypto/catalog', cryptoController.getAllCrypto);
 //OTHER
 
 module.exports = router;
