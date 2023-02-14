977aa27ef04e579285994bace666873789b4df58
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-14 19:13:58 +0100

    authorization middleware added

--- src/config/routes.js
+++ src/config/routes.js
@@ -6,6 +6,7 @@ const router = Router();
 const homeController = require('../controllers/homeController');
 const userController = require('../controllers/userController');
 const cryptoController = require('../controllers/cryptoContorller');
+const { isAuthenticated }= require('../middleware/authMiddleware');
 //TO DO: Endpoints and actions 
 
 //HOME
@@ -13,12 +14,15 @@ router.get('/', homeController.getHome);
 //AUTH-USER
 //Login
 router.get('/users/login', userController.getLogin);
+router.post('/users/login', userController.postLogin);
 //Register
 router.get('/users/register', userController.getRegister);
+router.post('/users/register', userController.postRegister);
 //Logout
+router.get('/users/logout', isAuthenticated,  userController.getLogout);
 //CRYPTO
 router.get('/crypto/catalog', cryptoController.getAllCrypto);
-router.get('/crypto/createOffer', cryptoController.getCreateOffer);
+router.get('/crypto/createOffer',isAuthenticated, cryptoController.getCreateOffer);
 //OTHER
 
 module.exports = router;
