8cdbf8b999043dfadd3f2c988a2dbf6098b12eab
kremver6 <102729993+dobriNedev@users.noreply.github.com>
2023-02-15 10:01:58 +0100

    POST create offer

--- src/controllers/cryptoContorller.js
+++ src/controllers/cryptoContorller.js
@@ -1,9 +1,22 @@
-
+const cryptoService = require('../services/cryptoService');
+const { getErrorMessage } = require('../utils/errorUtil');
 
 exports.getAllCrypto = async(req, res) => {
     res.render('catalog');
 };
 
-exports.getCreateOffer = async(req, res) => {
+exports.getCreateOffer = (req, res) => {
     res.render('create')
+};
+
+exports.postCreateOffer = async(req, res) => {
+    const { name, image, price, description, paymentMethod } = req.body;
+    const ownerId = res.locals.user._id;
+   
+    try {
+        await cryptoService.createCrypto(name, image, price, description, paymentMethod, ownerId);
+        res.redirect('/crypto/catalog');
+    } catch (error) {
+        res.status(401).render('create', { error: getErrorMessage(error) });
+    };
 };
